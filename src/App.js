eb7a629283d696802d80f71152fa317cecf37078
isaiah2004 <paulisaiah@karunya.edu.in>
2024-03-24 18:35:15 +0530

    change to react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'aframe'
+import React, {Component} from 'react'
+
+class App extends Component {
+  render () {
+    return (
+      <a-scene></a-scene>
+    );
+  }
+}
+
+export default App;
