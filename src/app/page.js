988380badc868f067eb2748b8b69bdf225bd8a01
RoshanJ20 <roshanjohn@karunya.edu.in>
2024-03-24 16:34:27 +0530

    working ;-;

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 import React from 'react';
 
-const AFrameScene = dynamic(() => import('../components/AFrameScene'), {
+const AFrameScene = dynamic(() => import('@/components/AFrameScene'), {
   ssr: false,
 });
 
--- src/components/AFrameScene.js
+++ src/components/AFrameScene.js
@@ -1,20 +1,24 @@
 "use client";
 // components/AFrameScene.js
 import 'aframe';
+import { Scene, Entity } from 'aframe-react';
 
 export default function AFrameScene() {
   return (
-    <a-scene>
-        
-      <a-assets>
-        <a-asset-item id="modelId" src="./assets/japanese_study_desk/scene.gltf"></a-asset-item></a-assets>
-        <a-entity gltf-model="#modelId" position="0 0 -5"  scale="0.5 0.5 0.5"></a-entity>
-      {/* <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box> */}
-      {/* <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere> */}
-      {/* <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder> */}
-      <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-      <a-sky color="#ECECEC"></a-sky>
-      
-    </a-scene>
+    <Scene>
+      <Entity
+        geometry={{ primitive: 'plane' }}
+        material={{ color: 'blue' }}
+        position={{ x: 0, y: -1, z: -5}}
+        rotation={{ x: 270, y: 0, z: 0 }}
+        scale={{ x: 5, y: 5, z: 5 }}
+
+        />
+      <Entity
+        geometry={{ primitive: 'box' }}
+        material={{ color: 'red' }}
+        position={{ x: 0, y: 0, z: -5 }}
+      />
+    </Scene>
   );
 }
