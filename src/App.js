e4025d8550df6e6cc1997c335932071c449d96c1
Paul_URK21CS1153_SATORI <paulisaiah@karunya.edu.in>
2024-03-25 23:35:10 +0530

    some scene setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import 'aframe'
 import React, {Component} from 'react'
+// my-app.js
+require('aframe-physics-system');
+require('aframe-physics-extras');
 
 class App extends Component {
   render () {
     return (
-      <a-scene></a-scene>
+      <a-scene>
+        <a-sky color="#3333aa"/>
+        <a-box position="0 0 -5" color="#563421"/>
+      </a-scene>
     );
   }
 }
